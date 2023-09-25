c221c435bd3fa66bcaba6652e03e492a2b7f4a21
Leonardo Rodrigues <leonardorodrigues@MacBook-Air-de-Leonardo.local>
2023-09-25 17:43:15 -0300

    feat: implemented not found path

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { router } from "./routes";
 import { errorHandler } from "./middlewares/error-handler.middleware";
@@ -10,5 +10,11 @@ app.use(cors());
 app.use(router);
 
 app.use(errorHandler);
+
+app.use((request: Request, response: Response, next: NextFunction) => {
+  return response.status(404).json({
+    error: "Not Found",
+  });
+});
 
 export { app };
